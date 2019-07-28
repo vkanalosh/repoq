46972c8ee10ed375ff1292a02f5bc92f269ee91c
vkanalosh <kanalosh.v@gmail.com>
2019-07-28 14:37:06 +0300

    real init

---
+++ ConsoleApp111/ConsoleApp111/ComplexNumber.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComplexNumbers
+{
+    public static class CN
+    {
+        public static ComplexNumber Add(ComplexNumber first, ComplexNumber other)
+        {
+            double a = first.A + other.A;
+            double b = first.B + other.B;
+            return new ComplexNumber(a, b);
+        }
+    }
+
+    public class ComplexNumber
+    {
+
+        public ComplexNumber(double a, double b)
+        {
+            A = a;
+            B = b;
+        }
+        public double A { get; set; }
+        public double B { get; set; }
+
+        public bool check {
+            get
+            {
+                return (this.B == 0);
+            }
+            set
+            { 
+                Console.WriteLine($"Value is {value}");
+            }
+        }
+        
+        public ComplexNumber Add(ComplexNumber other)
+        {
+            double a = this.A + other.A;
+            double b = this.B + other.B;
+            return new ComplexNumber(a, b);
+        }
+
+        public ComplexNumber Sub(ComplexNumber other)
+        {
+            double a = A - other.A;
+            double b = B - other.B;
+            return new ComplexNumber(a, b);
+        }
+
+        public ComplexNumber Multi(ComplexNumber other)
+        {
+            double a = (A * other.A - B * other.B);
+            double b = (A * other.B + B * other.A);
+            return new ComplexNumber(a, b);
+        }
+
+        public ComplexNumber Div(ComplexNumber other)
+        {
+
+            double a = ((A * other.A + B * other.B) / (other.A * other.A + other.B * other.B));
+            double b = (A * other.B - B * other.A) / (other.A * other.A + other.B * other.B);
+            return new ComplexNumber(a, b);
+        }
+
+        public void Result(ComplexNumber other)
+        {
+            Console.WriteLine($"number3 = {other.A} + {other.B}i");
+        }
+
+        public ComplexNumber AddDouble(double other)
+        {
+            double a = A + other;
+            double b = B;
+            return new ComplexNumber(a, b);
+        }
+
+        public ComplexNumber SubDouble(double other)
+        {
+            double a = A - other;
+            double b = B;
+            return new ComplexNumber(a, b);
+        }
+
+        public ComplexNumber MultiDouble(double other)
+        {
+            double a = (A * other - B * 0);
+            double b = (A * 0 + B * other);
+            return new ComplexNumber(a, b);
+        }
+
+        public ComplexNumber DivDouble(double other)
+        {
+            double a = ((A * other + B * 0) / (other * other + 0 * 0));
+            double b = (A * 0 - B * other) / (other * other + 0 * 0);
+            return new ComplexNumber(a, b);
+        }
+
+        public bool IsReallyComplex()
+        {
+            return  (this.B == 0);
+        }
+
+        public void SetIsReallyComplex(bool value)
+        {
+            Console.WriteLine($"Value is {value}");
+        }
+    }
+}
---
+++ ConsoleApp111/ConsoleApp111/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace ComplexNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a1, a2, b1, b2, a3;
+            string command;
+
+            Console.WriteLine("Put first number.");
+            a1 = Convert.ToDouble(Console.ReadLine());
+            b1 = Convert.ToDouble(Console.ReadLine());
+            
+            Console.WriteLine("Put second number.");
+            a2 = Convert.ToDouble(Console.ReadLine());
+            b2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Put not complex number.");
+
+            a3 = Convert.ToDouble(Console.ReadLine());
+            
+            Console.WriteLine("Put command: +, - , *, /, + not complex, - not complex, * not complex, / not complex.");
+            command = Console.ReadLine();
+           
+            ComplexNumber first = new ComplexNumber(a1, b1);
+            ComplexNumber second = new ComplexNumber(a2, b2);
+
+            switch (command)
+            {
+                case "+":
+                    ComplexNumber resultAdd = first.Add(second);
+                    ComplexNumber resultAdd2 = CN.Add(first, second);
+                    resultAdd.Result(resultAdd);
+
+                    bool result = resultAdd.IsReallyComplex();
+                    resultAdd.SetIsReallyComplex(false);
+
+                    bool result2 = resultAdd.check;
+                    resultAdd.check = false;
+
+                    Console.WriteLine(result);
+                    break;
+
+                case "-":
+                    ComplexNumber resultSub = first.Sub(second);
+                    resultSub.Result(resultSub);
+                    break;
+
+                case "*":
+                    ComplexNumber resultMulti = first.Multi(second);
+                    resultMulti.Result(resultMulti);
+                    break;
+
+                case "/":
+                    ComplexNumber resultDiv = first.Div(second);
+                    resultDiv.Result(resultDiv);
+                    break;
+
+                case "+ not complex":
+                    ComplexNumber resultAddDouble = first.AddDouble(a3);
+                    resultAddDouble.Result(resultAddDouble);
+                    break;
+
+                case "- not complex":
+                    ComplexNumber resultSubDouble = first.SubDouble(a3);
+                    resultSubDouble.Result(resultSubDouble);
+                    break;
+
+                case "* not complex":
+                    ComplexNumber resultMultiDouble = first.MultiDouble(a3);
+                    resultMultiDouble.Result(resultMultiDouble);
+                    break;
+
+                case "/ not complex":
+                    ComplexNumber resultDivDouble = first.DivDouble(a3);
+                    resultDivDouble.Result(resultDivDouble);
+                    break;
+
+            }
+
+            
+
+           
+
+            // топорно зробив, але парцює )
+            // створи 2 числа number1 i number2
+            // number3 це number1 + number2
+            // Console.WriteLine(number3.ToString())
+
+
+            // Дописати клас ComplexNumber:
+            // 1. Конструктор
+            // 2. в main створити 2 змінні цього типу і вивести їх суму як 3-у змінну.
+            //
+            // Додатково: Віднімання, Множення, Ділення, Операції порівняння
+            // Зчитування з консолі 2х чисел і вивести їх суму
+            // в ідеалі зчитувати спочатку що зробити (sum +, sub -, mul *, div /) а потім A i B на кожне з 2-х чисел.
+        }
+    }
+}
