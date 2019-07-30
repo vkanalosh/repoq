425aaabf990b3a0fd346e66e9cb402e43348d64e
vkanalosh <kanalosh.v@gmail.com>
2019-07-30 20:02:47 +0300

    Fractions

---
+++ Fractions/Fractions/Fractions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fractions
+{
+    class Fractions
+    {
+        public Fractions(long a, long b)
+        {
+            numerator = a;
+            denominator = b;
+        }
+
+        public long numerator {get; set;}
+        public long denominator {get; set;}
+
+       public Fractions Add(Fractions other)
+        {
+            long a = numerator + other.numerator;
+            long b = denominator;
+            return new Fractions(a, b);
+
+        }
+
+        public Fractions Sub(Fractions other)
+        {
+            long a = numerator - other.numerator;
+            long b = denominator;
+            return new Fractions(a, b);
+        }
+
+        public Fractions Multi(Fractions other)
+        {
+            long a = numerator * other.numerator;
+            long b = denominator * other.denominator;
+            return new Fractions(a, b);
+        }
+
+        public Fractions Div(Fractions other)
+        {
+            long a = numerator * other.denominator;
+            long b = denominator * other.numerator;
+            return new Fractions(a, b);
+        }
+
+        public void Result(Fractions other)
+        {
+            Console.WriteLine($"fractions 3 = {other.numerator}/{other.denominator}");
+        }
+    }
+}
---
+++ Fractions/Fractions/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Fractions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long a1, b1, a2, b2;
+            string comand;
+
+            Console.WriteLine("Vvedite pervii drob.");
+            a1 = Convert.ToInt64(Console.ReadLine());
+            b1 = Convert.ToInt64(Console.ReadLine());
+
+            Console.WriteLine("Vvedite vtoroi drob.");
+            a2 = Convert.ToInt64(Console.ReadLine());
+            b2 = Convert.ToInt64(Console.ReadLine());
+
+            Console.WriteLine("Vvedite komandu: +, -, *, /.");
+            comand = Console.ReadLine();
+
+            Fractions first = new Fractions(a1, b1);
+            Fractions second = new Fractions(a2, b2);
+
+            switch (comand)
+            {
+                case "+":
+                    Fractions resultAdd = first.Add(second);
+                    resultAdd.Result(resultAdd);
+                    break;
+
+                case "-":
+                    Fractions resultSub = first.Sub(second);
+                    resultSub.Result(resultSub);
+                    break;
+
+                case "*":
+                    Fractions resultMulti = first.Multi(second);
+                    resultMulti.Result(resultMulti);
+                    break;
+
+                case "/":
+                    Fractions resultDiv = first.Div(second);
+                    resultDiv.Result(resultDiv);
+                    break;
+                    
+
+                    
+            }
+
+
+
+
+
+        }
+    }
+}
