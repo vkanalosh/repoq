41e19ec2a370e0bf8d59c59e753d9c78684b621e
vkanalosh <kanalosh.v@gmail.com>
2019-09-26 20:46:35 +0300

    QrOvU8rn: Create new Program for tests

---
+++ BankTest/BankTest/BankArrayTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Bank;
+
+namespace BankTest
+{
+    class BankArrayTests
+    {
+        public void Add_BankAccountShouldNotFail()
+        {
+            BankArray arrayToTest = new BankArray();
+
+            for (int i = 0; i < 11; i++)
+            {
+                arrayToTest.Add($"Account{i}");
+            }
+
+            if (arrayToTest.Counter() != 11)
+            {
+                throw new Exception("Array should contain 11 elements after 11 additions.");
+            }
+        }
+    }
+}
---
+++ BankTest/BankTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BankTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                BankArrayTests bankArrayTests = new BankArrayTests();
+                bankArrayTests.Add_BankAccountShouldNotFail();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+}
